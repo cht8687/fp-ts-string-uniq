0387dfa6be6369b50512fe42d04ddcf59bef2059
Robert Chang <cht8687@gmail.com>
2021-07-19 10:20:08 +1000

    init

--- index.ts
+++ index.ts
@@ -1,12 +1,4 @@
-import { filter, uniq as arrayUniq } from "fp-ts/lib/Array";
+import { uniq as arrayUniq } from "fp-ts/lib/Array";
 import { Eq } from "fp-ts/string"
-
-export const split = (char: string) => (string: string) => string.split(char);
-
-export const join = (char: string) => (strings: string[]) => strings.join(char);
-
-export const filterEmptyString = filter((a: string) => a.length > 0);
-
-export const startsWith = (a: string) => (b: string) => b.startsWith(a)
 
 export const uniq = arrayUniq(Eq)
--- test.ts
+++ test.ts
@@ -1,14 +1,6 @@
-import { split, join, uniq } from './'
+import { uniq } from './'
 import { pipe } from 'fp-ts/lib/pipeable'
 import * as assert from 'assert'
-
-const result = pipe(
-    "a,b,c",
-    split(','),
-    join(':')
-)
-
-assert.deepStrictEqual(result, "a:b:c")
 
 
 const uniqTestResult = pipe(
